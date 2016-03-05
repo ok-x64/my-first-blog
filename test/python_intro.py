de1cc09194039fabf9dcd6a30ef42c5492530ed9
ok-x64 <olga.krivchikova@gmail.com>
2016-03-05 15:41:49 +0300

    My Django Girls first commit

---
+++ test/python_intro.py
@@ -0,0 +1,11 @@
+def hi(name):
+    print('Hi '+ name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+for i in range(98, 115):
+        print(i)
